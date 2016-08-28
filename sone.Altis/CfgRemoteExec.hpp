03d3f58c01e5e75b3e7656ce278b9acaa60f1b98
tommy <tgrondal@gmail.com>
2016-08-28 17:14:54 +0200

    remoteexec

--- sone.Altis/CfgRemoteExec.hpp
+++ sone.Altis/CfgRemoteExec.hpp
@@ -267,6 +267,17 @@ class CfgRemoteExec {
         F(life_fnc_setFuel,ANYONE)
         F(life_fnc_simDisable,ANYONE)
         F(SPY_fnc_notifyAdmins,ANYONE)
+		
+		class AR_Client_Rappel_From_Heli { allowedTargets=0; }; 
+        class AR_Hint { allowedTargets=1; }; 
+        class AR_Hide_Object_Global { allowedTargets=2; }; 
+        class AR_Enable_Rappelling_Animation { allowedTargets=2; }; 
+        class AR_Rappel_From_Heli { allowedTargets=2; }; 
+		
+		class AUR_Hint { allowedTargets=1; }; 
+        class AUR_Hide_Object_Global { allowedTargets=2; }; 
+        class AUR_Enable_Rappelling_Animation_Global { allowedTargets=2; }; 
+        class AUR_Play_Rappelling_Sounds_Global { allowedTargets=2; }; 
 
     };
 
